953e3341dfad6a7a07b276dfb1cb706f942eb6b2
detollly <anatoly.demianov@gmail.com>
2025-03-04 14:48:25 +0000

    added tailwind

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import MyRoutes from './components/routing/MyRoutes';
 import MenuItem from "./components/MenuItem"
-import styled from 'styled-components'
-
 
 function App() {
   return (
-    <AppCSS>
-      <MyRoutes/>
-    </AppCSS>
+    <MyRoutes />
   );
 }
-
 export default App;
-
-const AppCSS = styled.div `
-
-  box-sizing: border-box;
-
-  border: solid purple 1px; /* Debugging */
-
-  //width: 100vh;
-  height: 100vh; 
-
-
-`
