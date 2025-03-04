67f04b639980cfd1b781534ab23535351464de46
detollly <anatoly.demianov@gmail.com>
2025-03-04 18:33:20 +0000

    global colour variables are set

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import MyRoutes from './components/routing/MyRoutes';
-import MenuItem from "./components/MenuItem"
+import MenuItem from "./components/MenuItem";
+import './output.css';
 
 function App() {
   return (
-    <MyRoutes />
+    <div>
+      <MyRoutes />
+      <div className="bg-background text-primary p-4">
+      <h1 className="text-secondary">Hello, Tailwind!</h1>
+      <p className="text-primary">This is a paragraph with custom colors.</p>
+    </div>
+      </div>
   );
 }
 export default App;
