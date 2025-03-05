392f80db092959483a65f9307ad8a0ff063eca97
Mumonthebrink <mumonthebrink@gmail.com>
2025-03-05 13:46:28 +0000

    only homepage

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -4,9 +4,17 @@ const HomePage = () => {
 
     return(
         <>
-        <h1>Homepage</h1>
+        <h1>BioMorph Bistro</h1>
+        <h2>A Culinary Illusion, Naturally Printed</h2>
+        <p>Step into a world where innovation meets nature. At Biomorph Bistro, we craft delicious, 3D-printed organic meals using sustainably sourced, natural ingredients. Inspired by the intelligence of mycelium networks, our menu is designed to nourish both body and planet, offering nutrient-rich, eco-friendly cuisine that supports gut health, immunity, and sustainable living. </p>
+        <p>Experience a fusion of science and flavor, where every bite is a step towards a healthier, greener future. Join us in redefining food—one biomorphic creation at a time!</p>
         </>
-    )
+        <p>[placeholder for video]</p>
+        <p>Explore our innovative and natural menu</p>
+        <p>[placeholder for explore menu button]</p>
+        </>
+        // here comes a button to explore menu
+        )
 } 
 
 export default HomePage
