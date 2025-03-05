52ed333555ddae0d4b8db68bd334de0cdf94c7f3
Dylan Evelyn (DJEvelyn) <DylanJamesEvelyn@hotmail.co.uk>
2025-03-05 13:54:14 +0000

    HomePage small fix

--- src/components/pages/HomePage.jsx
+++ src/components/pages/HomePage.jsx
@@ -8,7 +8,6 @@ const HomePage = () => {
         <h2>A Culinary Illusion, Naturally Printed</h2>
         <p>Step into a world where innovation meets nature. At Biomorph Bistro, we craft delicious, 3D-printed organic meals using sustainably sourced, natural ingredients. Inspired by the intelligence of mycelium networks, our menu is designed to nourish both body and planet, offering nutrient-rich, eco-friendly cuisine that supports gut health, immunity, and sustainable living. </p>
         <p>Experience a fusion of science and flavor, where every bite is a step towards a healthier, greener future. Join us in redefining food—one biomorphic creation at a time!</p>
-        </>
         <p>[placeholder for video]</p>
         <p>Explore our innovative and natural menu</p>
         <p>[placeholder for explore menu button]</p>
