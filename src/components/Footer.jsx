81d19ee54c4b32fb471a6ac51c615e429063c202
detollly <anatoly.demianov@gmail.com>
2025-03-07 20:35:27 +0000

    added global fonts

---
+++ src/components/Footer.jsx
@@ -0,0 +1,19 @@
+import react from 'react'
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+import logo from '../images/logo.png';
+
+const Footer = () => {
+
+    return (
+        <FooterCSS>
+
+        </FooterCSS>
+    )
+}
+
+export default Footer
+
+const FooterCSS = styled.div`
+
+`;
